bb93e028beb503a25988bfbf7256bbfde998fbc3
Pedro Henrique Albuquerque <pedrohalb05@gmail.com>
2025-02-24 21:41:05 -0300

    Aula01

--- Atividades/Aula01/Aula01/Controllers/HomeController.cs
+++ Atividades/Aula01/Aula01/Controllers/HomeController.cs
@@ -15,7 +15,9 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        Variaveis minhavariavel = new Variaveis();
+
+        return View(minhavariavel);
     }
 
     public IActionResult Privacy()
---
+++ Atividades/Aula01/Aula01/Models/Variaveis.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula01.Models
+{
+	public class Variaveis
+	{
+		//Tipo implicito	
+		// var teste = 10;
+			
+		//Anotação de tipos
+		public int userCount = 10;
+
+		//Uma variavel pode ser declarada e não inicializada
+		public int totalCount;
+
+		//Constantes
+		//Para declarar uma constante, utilizamos a palavra-chave CONST, no entanto, a CONST deve ser inicializada quando declarada
+		public const int interestRate = 10;
+
+
+		//O  método construtor é invocado na instanciação do objeto por meio da palavra reservad new
+		//Por regra, o construtor sempre tem o mesmo nome da classe
+		//Quando tem parenteses () é procedimento, colchetes [] é vetores.
+		public Variaveis()
+		{
+			totalCount = 0;
+
+			//Tipo implicito, a palavra chave Var, se encarrega de definir o tipo de variavel na instrução de atribuição (após sinal de =)
+			var signalStrength = 22;
+			var companyName = "ACME";
+
+
+		}
+
+
+
+
+	}
+}
