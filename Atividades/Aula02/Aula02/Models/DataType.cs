cac6bb81762c060e21f3659b253e8f8c84085e6b
Pedro Henrique Albuquerque <pedrohalb05@gmail.com>
2025-03-17 21:16:36 -0300

    Aula04

---
+++ Atividades/Aula02/Aula02/Models/DataType.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace Aula02.Models
+{
+	public class DataType
+	{
+		public char myVar = 'a';
+		public char myConst = 'b';
+
+		public char myChar1 = 'f';
+		public char myChar2 = ':';
+		public char myChar3 = 'x';
+
+		//Podemos também atribuir referência de caracteres Unicode
+		public char myChar4 = '\u2726';
+
+		//Podemos ainda mesclar caracteres especiais como,
+		//nova linha, tabulação e etc..
+		public string textLine = "Esta é uma linha de texto.\n\n\nE está é outra linha";
+
+		/*
+		\e - Caracter de escape
+		\n - Nova linha
+		\t - Tabulação horizontal
+		\" - Aspas duplas, usadas para exibir aspas dentro de string
+		\' - Aspas simples, usados para exibir aspas simples na string		 
+		*/
+
+		private int count = 10;
+		public string message;
+
+		public DataType()
+		{
+			//Interpolação de Strings
+			//Combinando Strings de diferentes maneiras no C#
+			message = $"O contador está em {count}";
+
+			string username = "Pedro";
+			int inboxCount = 10;
+			int maxCount = 100;
+
+			message += $"\nO usuário {username} tem {inboxCount} mensagens.";
+			message += $"\nEspaço restante em sua caixa {maxCount - inboxCount}.";
+		}
+
+	}
+}
---
+++ Atividades/Aula02/Aula02/Models/TypeCasting.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula02.Models
+{
+	public class TypeCasting
+	{
+		//Declaranado variáveis na classe
+		public int myInteger = 20;
+		public long myLong;
+
+		public string myType1;
+		public string myType2;
+
+
+		public TypeCasting()
+		{
+			//Conversão implícita de tipos
+			myLong = myInteger;
+			//myInteger = myLong;
+			//Neste caso, long é muito grande para o int e a conversão implícit não é permitida.
+
+			//Conversão explicita
+			long myLong2 = 138129210;
+			int myInteger2;
+			myInteger2 = (int)myLong2;
+
+			//É possível identificar qual é o tipo de uma variável em tempo de execução
+			myType1 = myLong2.GetType().ToString();
+			myType2 = myInteger2.GetType().ToString();
+
+		}
+	}
+}
