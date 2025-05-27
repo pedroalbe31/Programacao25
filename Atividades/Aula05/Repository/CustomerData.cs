cdb8b10a220a05be900c2e6e4213914fe799cb15
Pedro Henrique Albuquerque <pedrohalb05@gmail.com>
2025-05-26 21:43:23 -0300

    aula2605

---
+++ Atividades/Aula05/Repository/CustomerData.cs
@@ -0,0 +1,17 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+	public class CustomerData
+	{
+		public static List<Customer> Customers { get; set; } = [];
+		public static List<Product> Products { get ; set ; } = [];
+		public static List<Order> Orders { get ; set ; } = [];
+	}
+}
--- Atividades/Aula05/Repository/CustomerRepository.cs
+++ Atividades/Aula05/Repository/CustomerRepository.cs
@@ -1,20 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Modelo;
+﻿using Modelo;
 
 namespace Repository
 {
 	public class CustomerRepository
 	{
-		public Customer Retrieve(int orderId)
+		public Customer Retrieve(int Id) //(buscar por ID)
 		{
-			return new Customer();
+			foreach (Customer c in CustomerData.Customers)
+				if (c.Id == Id)
+					return c;
+
+			return null!;
 		}
+
+		public List<Customer> RetriveByName(string name)  //(buscar por nome)
+		{
+			List<Customer> ret = new List<Customer>();
+
+			foreach (Customer c in CustomerData.Customers) 
+
+				if (c.Name!.ToLower().Contains(name.ToLower()))
+					ret.Add(c);
+
+			return ret;
+		}
+
+		public List<Customer> RetrieveAll()
+		{
+			return CustomerData.Customers;
+		}
+
 		public void Save(Customer customer)
 		{
+			customer.Id = Getcount() + 1;
+			CustomerData.Customers.Add(customer);
+		}
+
+		public bool Delete(Customer customer)
+		{
+			 return CustomerData.Customers.Remove(customer);
+		}
+
+		public bool DeleteById(int id)
+		{ 
+			Customer customer = Retrieve(id);
+
+			if (customer != null)
+				 return Delete(customer);
+			 return false;
+		}
+
+		public void Update(Customer newCustomer)
+		{
+			Customer oldCustomer = Retrieve(newCustomer.Id);
+			oldCustomer.Name = newCustomer.Name;
+			oldCustomer.WorkAdress = newCustomer.WorkAdress;
+			oldCustomer.HomeAdress = newCustomer.HomeAdress;
+		}
+
+		public int Getcount()
+		{
+			return CustomerData.Customers.Count;
 		}
 	}
 }
