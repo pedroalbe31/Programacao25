74532ff4ede7331c6957888c0302b1b5d9177995
Pedro Henrique Albuquerque <pedrohalb05@gmail.com>
2025-06-02 21:21:05 -0300

    Delimitado

--- Atividades/Aula05/Aula05/Controllers/CustomerController.cs
+++ Atividades/Aula05/Aula05/Controllers/CustomerController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Modelo;
 using Repository;
+using System.IO;
 
 namespace Aula05.Controllers
 {
 	public class CustomerController : Controller
 	{
+		private readonly IWebHostEnvironment environment;
+
 		private CustomerRepository _customerRepository;
 
-		public CustomerController()
+		public CustomerController(
+			IWebHostEnvironment environment
+		)
 		{
 			_customerRepository = new CustomerRepository();
+			this.environment = environment;
 		}
 
 		[HttpGet]
@@ -34,6 +40,54 @@ namespace Aula05.Controllers
 		[HttpGet]
 		public IActionResult Create()
 		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult ExportDelimitatedFile()
+		{
+			string fileContent = string.Empty;
+
+			foreach (Customer c in CustomerData.Customers)
+			{
+				fileContent += 
+					@$"{c.Id};
+						{c.Name};
+						{c.HomeAdress!.Id};
+						{c.HomeAdress.City};
+						{c.HomeAdress.State};
+						{c.HomeAdress.Country};
+						{c.HomeAdress.StreetLine1};
+						{c.HomeAdress.StreetLine2};
+						{c.HomeAdress.PostalCode};
+						{c.HomeAdress.AddressType}
+						\n
+					";
+			}
+
+			var path = Path.Combine(
+				environment.WebRootPath,
+				"TextFiles"
+			);
+
+			if(!System.IO.Directory.Exists(path))
+				System.IO.Directory.CreateDirectory(path);
+
+
+			var filepath = Path.Combine(
+				path,
+				"Delimitado.txt"
+			);
+
+			if (!System.IO.File.Exists(filepath))
+			{
+				using (StreamWriter sw = System.IO.File.CreateText(filepath))
+				{
+					sw.Write(fileContent);
+				}
+			}
+
+
 			return View();
 		}
 	}
--- Atividades/Aula05/Aula05/Program.cs
+++ Atividades/Aula05/Aula05/Program.cs
@@ -1,3 +1,7 @@
+using System.Runtime.CompilerServices;
+using Modelo;
+using Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,5 +29,30 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+FillCustomerData();
 
 app.Run();
+
+static void FillCustomerData()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Customer customer = new()
+        {
+           Id = i,
+           Name = $"Customer {i}",
+           HomeAdress = new Address()
+           {
+               Id = i,
+               AddressType = "Casa",
+               City = "CamposNovos",
+               Country = "Brazil",
+               PostalCode = "89620-000",
+               State = "SC",
+               StreetLine1 = "Rua da minha casa",
+               StreetLine2 = "Sua casa"
+           }
+        };
+        CustomerData.Customers.Add(customer);
+    }
+}
